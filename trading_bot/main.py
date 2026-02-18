68f22afc8bd9b740ccab8b3f3ddbe0dca3a20485
Maikel <adrianandresarjonaavila@gmail.com>
2026-02-18 17:49:21 -0500

    Actualizar main.py

--- trading_bot/main.py
+++ trading_bot/main.py
@@ -1,1 +1,12 @@
+import random
+import time
+
+def get_price():
+    return round(random.uniform(90, 110), 2)
+
 print("Trading bot iniciado correctamente 🚀")
+
+while True:
+    price = get_price()
+    print(f"Precio actual: ${price}")
+    time.sleep(2)
